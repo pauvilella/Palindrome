f0a4044e628f35691f80601e2b8c6488e4af65ac
pauvilella <pau.vilella.st@gmail.com>
2021-01-30 14:58:46 +0100

    Add the solution

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ Exercise related with Linked Lists.
 Implement a function to check if a linked list is a palindrome.
 
 ### Solution
+Both solutions perform the same algorithm but the first one is implemented with a reverse iterator and the second one by reversing the linked list.
+Basically, what I do is check if the elements are the same checked forward than backwards (in the first solution by using the reversed iterator and the second one by comparing the first linked list with the reversed linked list).
+This solution runs in O(N) runtime, which is the BCR of this problem.
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,46 @@
+import java.util.Iterator;
+import java.util.LinkedList;
+
 public class Main {
 
+    static boolean checkPalindrome1(LinkedList<Character> list) {
+        Iterator it = list.descendingIterator();
+        Iterator it2 = list.iterator();
+        while(it.hasNext()) {
+            if(!it.next().equals(it2.next())) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+    static boolean checkPalindrome2(LinkedList<Character> list) {
+        LinkedList<Character> aux = new LinkedList<>();
+        Iterator it = list.iterator();
+        while(it.hasNext()) {
+            aux.addFirst((Character) it.next());
+        }
+        it = list.iterator();
+        Iterator it2 = aux.iterator();
+        while(it.hasNext()) {
+            if(!it.next().equals(it2.next())) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        LinkedList<Character> list = new LinkedList<>();
+        list.add('a');
+        list.add('b');
+        list.add('c');
+        list.add('d');
+        list.add('f');
+        list.add('c');
+        list.add('b');
+        list.add('a');
+        System.out.println(checkPalindrome1(list));
+        System.out.println(checkPalindrome2(list));
+    }
 }
